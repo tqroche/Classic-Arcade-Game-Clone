f3f3062e76c226db85381932a7aa9d56916b5206
Tchanavian Roche <54754871+tqroche@users.noreply.github.com>
2019-12-05 16:04:42 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
+(function(){
+
+
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+let Enemy = function Enemy(x, y, s) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = s;
 };
-
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -19,22 +21,28 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
-
-    // when off canvas, reset position of enemy to move across again
-    if (this.x > 550) {
-        this.x = -100;
-        this.speed = 100 + Math.floor(Math.random() * 512);
+    if (this.x > 505) {
+      this.x = -100;
+      let someSpeed = Math.floor(Math.random() * 4 + 1);
+      this.speed = 60 * someSpeed;
     }
 
-    // Check for collision between player and enemies
-    if (player.x < this.x + 60 &&
-        player.x + 37 > this.x &&
-        player.y < this.y + 25 &&
-        30 + player.y > this.y) {
-        player.x = 200;
-        player.y = 380;
-        checkVictory();s
+    let enemyXLeftMax = this.x - 70;
+    let enemyXRightMax = this.x + 70;
+    let enemyYTopMax = this.y - 60;
+    let enemyYBottomMax = this.y + 60;
+    if (player.x > enemyXleftMax && player.x < enemyXRightMax && player.y > enemyYTopMax && player.y < enemyYBottomMax) {
+
+    player.resetPosition();
+    lives--;
+    updateView('you died. ' + lives + ' live(s) remaining...');
+    if (lives === 0) {
+      alert('game over...');
+      player.resetPosition();
+      is_game_over = true;
+      updateView('you died. ' + lives + 'live(s) remaining...');
     }
+  }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -45,112 +53,103 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/char-boy.png';
+
+const Player = function() {
+  this.sprite = "images/char-boy.png";
+  this.x = 303;
+  this.y = 404;
+  this.h_step = 101;
+  this.v_step = 83;
 };
 
-Player.prototype.update = function() {
-    // Prevent player from moving beyond canvas wall boundaries
-    if (this.y > 380) {
-        this.y = 380;
-    }
-
-    if (this.x > 400) {
-        this.x = 400;
-    }
-
-    if (this.x < 0) {
-        this.x = 0;
-    }
-
-    // Check for player reaching top of canvas and winning the game
-    if (this.y < 0) {
-        this.x = 200;
-        this.y = 380;
-    }
+Player.prototype.resetPosition = function() {
+  this.x = 303;
+  this.y = 404;
 };
 
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(keyPress) {
-    switch (keyPress) {
-        case 'left':
-            this.x -= this.speed + 50;
-            break;
-        case 'up':
-            this.y -= this.speed + 30;
-            break;
-        case 'right':
-            this.x += this.speed + 50;
-            break;
-        case 'down':
-            this.y += this.speed + 30;
-            break;
-    }
+Player.prototype.handleInput = function(direction) {
+  switch (direction) {
+    case 'left':
+    this.x >= this.h_step ? this.x -= this.h_step : this.x -= 0;
+      break;
+    case 'right':
+      this.x <= (this.h_step * 5) ? this.x += this.h_step : this.x +=0;
+      break;
+    case 'up':
+      this.y -= this.v_step;
+      if(this.y <= 50) {
+        score += 10;
+        crossed++;
+        updateView('you win! score: ' + score);
+        window.gem = new Gem();
+        if(crossed % 5 ===0) {window.heart = new Heart(); }
+        this.resetPosition();
+      }
+      break;
+    case 'down':
+        this.y <= (this.v_step * 4) ? this.y += this.v_step : this.y += 0;
+        break;
+  }
 };
-
-// Check if player reached river
-    function checkVictory() {
-        if (victory()) {
-            game.board.paintNextFrame = false;
-        }
-
-        // Check if player reached the water
-        function victory() {
-            if (game.player.y === game.board.TOP_WALL) {
-                return true;
-            }
-        }
-    }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
 
-// Position "y" where the enemies will are created
-var enemyPosition = [60, 140, 220];
-var player = new Player(200, 380, 50);
-var enemy;
+const players = [
+  'images/char-boy.png',
+  'images/char-cat-girl.png',
+  'images/char-horn-girl.png',
+  'images/char-pink-girl.png',
+  'images/char-princess-girl.png',
+  'images/char-boy.png'
+];
 
-enemyPosition.forEach(function(posY) {
-    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
-    allEnemies.push(enemy);
-});
+const Selector = function Selector() {
+    this.image = 'images/Selector.png';
+
+    this.x = 606;
+    this.y = 375;
+  }
+  Selector.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.image), this.x, this.y);
+  };
+  Selector.prototype.stepped_on = function() {
+    var leftMax = this.x - 40;
+    var rightMax = this.x + 40;
+    var topMax = this.y - 40;
+    var bottomMax = this.y + 40;
+
+    if(player.x > leftMax && player.x < rightMax && player.y > topMax && player.y < bottomMax) {
+      player.sprite = players[ Math.floor(Math.random() * players.length) ];
+      updateView('character changed!');
+      player.resetPosition();
+    }
+  }
+
+
+  // Place the player object in a variable called player
+  window.player = new Player();
+  window.selector = new Selector();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
 
+  if (allowedKeys[e.keyCode]){
     player.handleInput(allowedKeys[e.keyCode]);
+  }
 });
 
-// Handle victory modal's on/off state
-function toggleVictoryModal() {
-  const modal = document.querySelector('.modal');
-  const modalRestartBtn = document.querySelector(".modal__button");
-  modalRestartBtn.addEventListener("click", onRestart);
-  modal.classList.toggle('hide');
-}
 
-// Reset hero back to starting coords
-class Hero {
-    constructor(x, y, sprite) {
-        this.startX = x;
-        this.startY = y;
-        this.x = this.startX;
-        this.y = this.startY;
-        this.sprite = sprite;
-    }
-}
+})()
